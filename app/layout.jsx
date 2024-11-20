d01ab8763c474f2810d47c9c71f0021133bdeb65
Hamilton Huang <hamilton@Hamiltons-MacBook-Air.local>
2024-11-20 10:02:12 +0800

    init fonts menu

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,8 @@
 //import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition"
 /*
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +32,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>
+          {children}
+        </PageTransition>
       </body>
     </html>
   );
